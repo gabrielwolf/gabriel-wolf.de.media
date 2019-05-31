6afb2634173373533e2951c1d62da1f10dd96062
Gabriel Wolf <gabriel.wolf@wolfzeit.com>
2019-06-01 00:10:53 +0200

    Standardised naming of assert functions

--- test/tests.py
+++ test/tests.py
@@ -6,32 +6,31 @@ from src.main import sane_file_extensions, sane_file_naming_schema
 class FileNamingTests(TestCase):
 
     def test_sane_file_extensions_positive(self):
-        self.assertIs(sane_file_extensions(['test.txt'], ['.txt']), True)
+        self.assertTrue(sane_file_extensions(['test.txt'], ['.txt']))
 
     def test_sane_file_extensions_negative(self):
-        self.assertIs(sane_file_extensions(['test.exe'], ['.txt']), False)
+        self.assertFalse(sane_file_extensions(['test.exe'], ['.txt']))
 
     def test_sane_file_extensions_many_extensions_positive(self):
-        self.assertIs(sane_file_extensions(['test.txt', 'test.jpg'], ['.txt', '.jpg']), True)
+        self.assertTrue(sane_file_extensions(['test.txt', 'test.jpg'], ['.txt', '.jpg']))
 
     def test_sane_file_extensions_many_extensions_negative(self):
-        self.assertIs(sane_file_extensions(['test.exe'], ['.txt', '.jpg']), False)
+        self.assertFalse(sane_file_extensions(['test.exe'], ['.txt', '.jpg']))
 
     def test_sane_file_naming_schema_simple_positive(self):
-        self.assertIs(sane_file_naming_schema(['2019-05-27_Sonnenuntergang in Petershausen.jpg']), True)
+        self.assertTrue(sane_file_naming_schema(['2019-05-27_Sonnenuntergang in Petershausen.jpg']))
 
     def test_sane_file_naming_schema_simple_negative(self):
-        self.assertIs(sane_file_naming_schema(['2019-99-27_Sonnenuntergang in Petershausen.jpg']), False)
+        self.assertFalse(sane_file_naming_schema(['2019-99-27_Sonnenuntergang in Petershausen.jpg']))
 
     def test_sane_file_naming_schema_extended_positive(self):
-        self.assertIs(sane_file_naming_schema(['2019-05-27_20-41-36_Sonnenuntergang in Petershausen.jpg']), True)
+        self.assertTrue(sane_file_naming_schema(['2019-05-27_20-41-36_Sonnenuntergang in Petershausen.jpg']))
 
     def test_sane_file_naming_schema_extended_negative(self):
-        self.assertIs(sane_file_naming_schema(['2019-05-27_29-99-36_Sonnenuntergang in Petershausen.jpg']), False)
+        self.assertFalse(sane_file_naming_schema(['2019-05-27_29-99-36_Sonnenuntergang in Petershausen.jpg']))
 
     def test_sane_file_naming_schema_more_underscores_no_problem(self):
-        self.assertIs(sane_file_naming_schema(['2019-05-27_21-50-36_Sonnenuntergang in Petershausen_schön.jpg']), True)
+        self.assertTrue(sane_file_naming_schema(['2019-05-27_21-50-36_Sonnenuntergang in Petershausen_schön.jpg']))
 
     def test_sane_file_naming_schema_startswith_underscore_negative(self):
-        self.assertIs(sane_file_naming_schema(['_2019-05-27_21-50-36_Sonnenuntergang in Petershausen_schön.jpg']),
-                      False)
+        self.assertFalse(sane_file_naming_schema(['_2019-05-27_21-50-36_Sonnenuntergang in Petershausen_schön.jpg']))
