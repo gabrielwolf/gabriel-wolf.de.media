c9696e2249cce506f388791aa28765af165f6833
Gabriel Wolf <gabriel.wolf@wolfzeit.com>
2019-05-31 23:11:04 +0200

    Fix and refactorig

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ import os
 from datetime import datetime
 
 media_files_directory = "../media/"
+allowed_extensions = [".txt", ".jpg", ".mp4", ".wav"]
 
 
 def read_dir(directory, hidden_files_prefixes):
@@ -16,56 +17,57 @@ def read_dir(directory, hidden_files_prefixes):
     return files
 
 
-def dir_sane_file_extensions(directory, extensions):
+def sane_file_extensions(files, extensions):
     """
-    returns False for directories that contain other files, that have other extensions than given.
-    Dotfiles are not of concern.
+    returns False for lists that contain file names with other extensions than given.
+    :param files: list of file name strings
+    :param extensions: list of extensions strings e.g. [".jpg", ".txt"]
     """
-    files = os.listdir(directory)
     for file in files:
-        if not file.startswith("."):
-            filename, extension = os.path.splitext(file)
-            if extension not in extensions:
-                return False
+        filename, extension = os.path.splitext(file)
+        if extension not in extensions:
+            return False
     return True
 
 
-def dir_sane_file_naming_schema(directory):
+def sane_file_naming_schema(files):
     """
-    returns False for directories that contain files, that don't match a scheme.
-    Files beginning with _ are being ignored. They are considered to be temporarily disabled.
+    returns False for lists that contain file names, that don't match the scheme.
+    File names mustn't start with underscore.
+    Time is optional, just omit _HH-MM-SS
 
     Scheme:  YYYY-MM-DD_HH-MM-SS_title.extension
     Example: 2019-05-27_20-44-16_Sonnenuntergang in Petershausen.jpg
 
-    Time is optional.
+    :param files: list of file name strings
     """
-    files = os.listdir(directory)
     for file in files:
-        if not file.startswith(".") and not file.startswith("_"):
-            filename, extension = os.path.splitext(file)
-            parts = filename.split("_")
-            if len(parts) == 1:
+        filename, extension = os.path.splitext(file)
+        parts = filename.split("_")
+        if len(parts) == 1:
+            # not a single underscore
+            return False
+        if len(parts) >= 2:
+            # good conditions for the simple scheme
+            try:
+                test = datetime.strptime(parts[0], "%Y-%m-%d")  # valid date
+            except ValueError:
+                print("Scheme not valid for file: ", file)
                 return False
-            if len(parts) == 2:
-                try:
-                    test = datetime.strptime(parts[0], "%Y-%m-%d")
-                except ValueError:
-                    print("Scheme not valid for file: ", file)
-                    return False
-            if len(parts) == 3:
-                try:
-                    test = datetime.strptime(parts[1], "%H-%M-%S")
-                except ValueError:
-                    print("Scheme not valid for file: ", file)
-                    return False
+        if len(parts) >= 3:
+            try:
+                test = datetime.strptime(parts[1], "%H-%M-%S")
+            except ValueError:
+                print("Scheme not valid for file: ", file)
+                return False
     return True
 
 
 def main():
-    print("extensions: " + str(dir_sane_file_extensions(media_files_directory, [".txt", ".jpg", ".mp4", ".wav"])))
-    print("schema: " + str(dir_sane_file_naming_schema(media_files_directory)))
-    read_dir("../", (".", "_"))
+    extension_test = sane_file_extensions(read_dir(media_files_directory, (".", "_", "Makefile")), allowed_extensions)
+    schema_test = sane_file_naming_schema(read_dir(media_files_directory, (".", "_", "Makefile")))
+    print("extensions: " + str(extension_test))
+    print("schema: " + str(schema_test))
 
 
 main()
--- test/tests.py
+++ test/tests.py
@@ -1,73 +1,33 @@
-import os
-import shutil
 from unittest import TestCase
 
-from src.main import dir_sane_file_extensions, dir_sane_file_naming_schema
-
-test_directory = './test/'
+from src.main import sane_file_extensions, sane_file_naming_schema
 
 
-def fill_a_test_directory(directory, test_file_names: list):
-    if not os.path.exists(directory):
-        os.makedirs(directory)
-    test_file_names = map(lambda file: directory + file, test_file_names)
-    # Write empty files to disk
-    for file in test_file_names:
-        open(file, 'a').close()
+class FileNamingTests(TestCase):
 
+    def test_sane_file_extensions_positive(self):
+        self.assertIs(sane_file_extensions(['test.txt'], ['.txt']), True)
 
-def rm_a_test_directory(directory):
-    shutil.rmtree(directory)
+    def test_sane_file_extensions_negative(self):
+        self.assertIs(sane_file_extensions(['test.exe'], ['.txt']), False)
 
+    def test_sane_file_extensions_many_extensions_positive(self):
+        self.assertIs(sane_file_extensions(['test.txt', 'test.jpg'], ['.txt', '.jpg']), True)
 
-class MediaFilesTests(TestCase):
+    def test_sane_file_extensions_many_extensions_negative(self):
+        self.assertIs(sane_file_extensions(['test.exe'], ['.txt', '.jpg']), False)
 
-    def test_dir_sane_file_extensions_positive(self):
-        fill_a_test_directory(test_directory, ['test.txt'])
-        self.assertIs(dir_sane_file_extensions(test_directory, ['.txt']), True)
-        rm_a_test_directory(test_directory)
+    def test_sane_file_naming_schema_simple_positive(self):
+        self.assertIs(sane_file_naming_schema(['2019-05-27_Sonnenuntergang in Petershausen.jpg']), True)
 
-    def test_dir_sane_file_extensions_negative(self):
-        fill_a_test_directory(test_directory, ['test.exe'])
-        self.assertIs(dir_sane_file_extensions(test_directory, ['.txt']), False)
-        rm_a_test_directory(test_directory)
+    def test_sane_file_naming_schema_simple_negative(self):
+        self.assertIs(sane_file_naming_schema(['2019-99-27_Sonnenuntergang in Petershausen.jpg']), False)
 
-    def test_dir_sane_file_extensions_dotfiles_dont_matter(self):
-        fill_a_test_directory(test_directory, ['.DS_Store'])
-        self.assertIs(dir_sane_file_extensions(test_directory, ['.txt']), True)
-        rm_a_test_directory(test_directory)
+    def test_sane_file_naming_schema_extended_positive(self):
+        self.assertIs(sane_file_naming_schema(['2019-05-27_20-41-36_Sonnenuntergang in Petershausen.jpg']), True)
 
-    def test_dir_sane_file_extensions_many_extensions_positive(self):
-        fill_a_test_directory(test_directory, ['test.txt', 'test.jpg'])
-        self.assertIs(dir_sane_file_extensions(test_directory, ['.txt', '.jpg']), True)
-        rm_a_test_directory(test_directory)
+    def test_sane_file_naming_schema_extended_negative(self):
+        self.assertIs(sane_file_naming_schema(['2019-05-27_29-99-36_Sonnenuntergang in Petershausen.jpg']), False)
 
-    def test_dir_sane_file_extensions_many_extensions_negative(self):
-        fill_a_test_directory(test_directory, ['test.exe'])
-        self.assertIs(dir_sane_file_extensions(test_directory, ['.txt', '.jpg']), False)
-        rm_a_test_directory(test_directory)
-
-    def test_dir_sane_file_naming_schema_simple_positive(self):
-        fill_a_test_directory(test_directory, ['2019-05-27_Sonnenuntergang in Petershausen.jpg'])
-        self.assertIs(dir_sane_file_naming_schema(test_directory), True)
-        rm_a_test_directory(test_directory)
-
-    def test_dir_sane_file_naming_schema_simple_negative(self):
-        fill_a_test_directory(test_directory, ['2019-23-27_Sonnenuntergang in Petershausen.jpg'])
-        self.assertIs(dir_sane_file_naming_schema(test_directory), False)
-        rm_a_test_directory(test_directory)
-
-    def test_dir_sane_file_naming_schema_extended_positive(self):
-        fill_a_test_directory(test_directory, ['2019-05-27_20-41-36_Sonnenuntergang in Petershausen.jpg'])
-        self.assertIs(dir_sane_file_naming_schema(test_directory), True)
-        rm_a_test_directory(test_directory)
-
-    def test_dir_sane_file_naming_schema_extended_negative(self):
-        fill_a_test_directory(test_directory, ['2019-05-27_29-99-36_Sonnenuntergang in Petershausen.jpg'])
-        self.assertIs(dir_sane_file_naming_schema(test_directory), False)
-        rm_a_test_directory(test_directory)
-
-    def test_dir_sane_file_naming_schema_more_underscores_no_problem(self):
-        fill_a_test_directory(test_directory, ['2019-05-27_29-99-36_Sonnenuntergang in Petershausen_schön.jpg'])
-        self.assertIs(dir_sane_file_naming_schema(test_directory), True)
-        rm_a_test_directory(test_directory)
+    def test_sane_file_naming_schema_more_underscores_no_problem(self):
+        self.assertIs(sane_file_naming_schema(['2019-05-27_21-50-36_Sonnenuntergang in Petershausen_schön.jpg']), True)
