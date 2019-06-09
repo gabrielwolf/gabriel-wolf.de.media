59eb679f013b2275586b2746d8f398cd072a77ad
Gabriel Wolf <gabriel.wolf@wolfzeit.com>
2019-06-09 20:52:43 +0200

    Right commands for jpg

--- src/convert_media.py
+++ src/convert_media.py
@@ -1,25 +1,32 @@
 #!/usr/local/bin/python3
-
 """
 This script converts media data of a given dir for web use.
 """
-
 import os
 
 from src.main import read_dir
 
 
-def get_output_file_names(file, extension):
+def get_shell_commands(file, extension):
     output = []
     if extension == ".jpg":
-        output.append(file.replace(".jpg", "-preload.jpg"))
-        output.append(file.replace(".jpg", "-400w.jpg"))
-        output.append(file.replace(".jpg", "-600w.jpg"))
-        output.append(file.replace(".jpg", "-800w.jpg"))
-        output.append(file.replace(".jpg", "-1000w.jpg"))
-        output.append(file.replace(".jpg", "-1500w.jpg"))
-        output.append(file.replace(".jpg", "-2000w.jpg"))
-        output.append(file.replace(".jpg", "-2500w.jpg"))
+        tool = "convert"
+        output.append(
+            [tool, file.replace(".jpg", "-preload.jpg"), "-strip -define -strip -geometry 400x -blur 0x20 -quality 1"])
+        output.append([tool, file.replace(".jpg", "-400w.jpg"),
+                       "-strip -define jpeg:dct-method=float -geometry 400x -sharpen 0x0.9 -quality 90"])
+        output.append([tool, file.replace(".jpg", "-600w.jpg"),
+                       "-strip -define jpeg:dct-method=float -geometry 600x -sharpen 0x0.9 -quality 90"])
+        output.append([tool, file.replace(".jpg", "-800w.jpg"),
+                       "-strip -define jpeg:dct-method=float -geometry 800x -sharpen 0x0.9 -quality 90"])
+        output.append([tool, file.replace(".jpg", "-1000w.jpg"),
+                       "-strip -define jpeg:dct-method=float -geometry 1000x -sharpen 0x0.9 -quality 90"])
+        output.append([tool, file.replace(".jpg", "-1500w.jpg"),
+                       "-strip -define jpeg:dct-method=float -geometry 1500x -sharpen 0x0.9 -quality 90"])
+        output.append([tool, file.replace(".jpg", "-2000w.jpg"),
+                       "-strip -define jpeg:dct-method=float -geometry 2000x -sharpen 0x0.9 -quality 90"])
+        output.append([tool, file.replace(".jpg", "-2500w.jpg"),
+                       "-strip -define jpeg:dct-method=float -geometry 2500x -sharpen 0x0.9 -quality 90"])
     return output
 
 
@@ -27,7 +34,7 @@ def is_done(file, extension, output_dir):
     had_to_be_done_files = []
     are_done_files = []
 
-    had_to_be_done_files.append(get_output_file_names(file, extension))
+    had_to_be_done_files.append(get_shell_commands(file, extension))
 
     try:
         output_files = read_dir(output_dir, (".", "_"))
@@ -45,13 +52,15 @@ def is_done(file, extension, output_dir):
         return True
 
     print(file + " is not (yet) web ready!")
+    print(file + " is not (yet) web ready!")
     return False
 
 
 def create(file, extension, input_dir, output_dir):
-    files_to_be_done = get_output_file_names(file, extension)
+    files_to_be_done = get_shell_commands(file, extension)
     for file_to_be_done in files_to_be_done:
-        print("convert %s/%s %s/%s" % (input_dir, file, output_dir, file_to_be_done))
+        print("%s %s %s/%s %s/%s" % (
+            file_to_be_done[0], file_to_be_done[2], input_dir, file, output_dir, file_to_be_done[1]))
 
 
 def convert_media(input_dir, output_dir):
