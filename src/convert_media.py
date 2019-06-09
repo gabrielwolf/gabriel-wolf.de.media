848c60d8e26b7f5260d3bc6d9b340e8d9723233a
Gabriel Wolf <gabriel.wolf@wolfzeit.com>
2019-06-09 19:43:15 +0200

    WIP cp would do, but still debugging

--- src/convert_media.py
+++ src/convert_media.py
@@ -9,11 +9,26 @@ import os
 from src.main import read_dir
 
 
+def get_output_file_names(file, extension):
+    output = []
+    if extension == ".jpg":
+        output.append(file.replace(".jpg", "-preload.jpg"))
+        output.append(file.replace(".jpg", "-400w.jpg"))
+        output.append(file.replace(".jpg", "-600w.jpg"))
+        output.append(file.replace(".jpg", "-800w.jpg"))
+        output.append(file.replace(".jpg", "-1000w.jpg"))
+        output.append(file.replace(".jpg", "-1500w.jpg"))
+        output.append(file.replace(".jpg", "-2000w.jpg"))
+        output.append(file.replace(".jpg", "-2500w.jpg"))
+    return output
+
+
 def is_done(file, extension, output_dir):
     had_to_be_done_files = []
-    if extension == ".jpg":
-        had_to_be_done_files.append(file)
     are_done_files = []
+
+    had_to_be_done_files.append(get_output_file_names(file, extension))
+
     try:
         output_files = read_dir(output_dir, (".", "_"))
         for output_file in output_files:
@@ -22,23 +37,27 @@ def is_done(file, extension, output_dir):
     except IOError:
         print("Could not read from disk!")
         raise
+
     had_to_be_done_files.sort()
     are_done_files.sort()
+
     if had_to_be_done_files == are_done_files:
         return True
+
     print(file + " is not (yet) web ready!")
     return False
 
 
-def create(file, extension, output_dir):
-    print(
-        "convert -strip -interlace JPEG -sampling-factor 4:2:0 -define jpeg:dct-method=float -geometry 400x -sharpen 0x0.9 -quality 90 input.jpg input-400x.jpg")
+def create(file, extension, input_dir, output_dir):
+    files_to_be_done = get_output_file_names(file, extension)
+    for file_to_be_done in files_to_be_done:
+        print("convert %s/%s %s/%s" % (input_dir, file, output_dir, file_to_be_done))
 
 
 def convert_media(input_dir, output_dir):
     try:
         input_files = read_dir(input_dir, (".", "_"))
-        output_files = read_dir(input_dir, (".", "_"))
+        output_files = read_dir(output_dir, (".", "_"))
     except IOError:
         print("Could not read from disk!")
         raise
@@ -46,7 +65,7 @@ def convert_media(input_dir, output_dir):
         filename, extension = os.path.splitext(file)
         if extension == ".jpg":
             if not is_done(file, extension, output_dir):
-                create(file, extension, output_dir)
+                create(file, extension, input_dir, output_dir)
 
 
 def main():
